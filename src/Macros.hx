42941a719d6146032516f85590415d8758cbcf52
Adrien Baudet <baudetad@gmail.com>
2021-10-27 15:05:11 +0200

    changed build macro into expression macro

--- src/Macros.hx
+++ src/Macros.hx
@@ -4,24 +4,14 @@ import haxe.macro.Context;
 import haxe.macro.Expr;
 #if macro
 import sys.io.File;
+import haxe.io.Path;
 #end
 
 class Macros {
-	macro public static function importStaticString(name:String, fileName:String):Array<Field> {
-		// get existing fields from the context from where build() is called
-		var fields = Context.getBuildFields();
-		var moduleArr = Context.getLocalClass().get().module.split(".");
-		moduleArr.pop();
-		var codePath = '${moduleArr.join("/")}/${fileName}';
+	macro public static function importString(fileName:String) {
+		var posInfos = Context.getPosInfos(Context.currentPos());
+		var fileContent = File.getContent(Path.join([Path.directory(posInfos.file), fileName]));
 
-		// append a field
-		fields.push({
-			name: name,
-			access: [Access.APublic, Access.AStatic, Access.AInline],
-			kind: FieldType.FVar(macro:String, macro $v{File.getContent(codePath)}),
-			pos: Context.currentPos(),
-		});
-
-		return fields;
+		return macro $v{fileContent};
 	}
 }
