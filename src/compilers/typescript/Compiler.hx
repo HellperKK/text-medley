23dd2395e27d03c585e6dd7638fd8ec2bd131e60
Adrien Baudet <baudetad@gmail.com>
2023-05-16 08:56:47 +0200

    add typescript compiler

---
+++ src/compilers/typescript/Compiler.hx
@@ -0,0 +1,62 @@
+package compilers.typescript;
+
+using Lambda;
+
+@:expose
+class Compiler extends BaseCompiler {
+	private var varNames:Array<String>;
+
+	public function new() {
+		super();
+
+		FUN_STD = Macros.importString("std.ts");
+		varNames = [];
+	}
+
+	public function blockList(blocks:Map<String, Block>):String {
+		var defs = [
+			for (block in blocks.keyValueIterator())
+				'function ${makeName(block.key)}():string {\n${indent(block.value.compile(this))}\n}'
+		];
+
+		return defs.join("\n\n");
+	}
+
+	public function constBlock(consts:Array<String>):String {
+		return consts.join("\n");
+	}
+
+	public function const(name:String, content:String):String {
+		if (varNames.indexOf(name) == -1) {
+			varNames.push(name);
+
+			return 'let ${name}:string = ${content};';
+		}
+
+		return '${name} = ${content};';
+	}
+
+	public function outputBlock(exps:Array<Expression>):String {
+		var lams = exps.mapi((index, exp) -> {
+			return 'if (_rand === ${index}) {\n${indent("return " + exp.compile(this) + ";")}\n}';
+			// return "(() => " + exp.compile(this) + ")";
+		}).join("\n");
+		return 'const _rand = randomNum(${exps.length});\n${lams}';
+	}
+
+	public function expression(exprs:Array<String>):String {
+		return exprs.join(" + ");
+	}
+
+	public function strToken(str:String):String {
+		return "\"" + Utils.escape(str) + "\"";
+	}
+
+	public function varToken(str:String):String {
+		return str;
+	}
+
+	public function blkToken(str:String):String {
+		return makeName(str) + "()";
+	}
+}
